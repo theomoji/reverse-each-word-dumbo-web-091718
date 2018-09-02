703b6219b0dec462a38e45e286a30e086fe93f96
Shuo Wang <gken90@gmail.com>
2018-09-02 19:25:15 +0000

    automatically backed up by learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(string)
+  new_string = ""
+  index = string.length
+while index > 0
+  new_string << string[index-1]
+  # new_string = string[index] + new_string #"e" + "h"
+  index -= 1
+end
