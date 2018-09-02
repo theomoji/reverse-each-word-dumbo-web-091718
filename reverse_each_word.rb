30189f9c8ac68c92d4486126cefbd3af33eda0fc
Shuo Wang <gken90@gmail.com>
2018-09-02 19:25:50 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,9 @@
 def reverse_each_word(string)
   new_string = ""
   index = string.length
-while index > 0
-  new_string << string[index-1]
+  while index > 0
+    new_string << string[index-1]
   # new_string = string[index] + new_string #"e" + "h"
-  index -= 1
-end
+    index -= 1
+  end
+end 
