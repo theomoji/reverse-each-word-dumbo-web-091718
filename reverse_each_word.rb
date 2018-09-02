5ba25b29063c2e47a81d867bf53cfea118e1f11a
Shuo Wang <gken90@gmail.com>
2018-09-02 20:55:24 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,11 +8,22 @@ def reverse_word(word)
   return new_string
 end
 
+# def reverse_each_word(sentence)
+#   string_arr = sentence.split(" ")
+#   reverse_arr = []
+#   string_arr.each do |word|
+#     reverse_arr << reverse_word(word)
+#   end
+#   return reverse_arr.join(" ")
+# end
+
+#a.collect { |x| x + "!" }        #=> ["a!", "b!", "c!", "d!"]
+
 def reverse_each_word(sentence)
   string_arr = sentence.split(" ")
-  reverse_arr = []
-  string_arr.each do |word|
+  # reverse_arr = []
+  reversed = string_arr.collect do |word|
     reverse_arr << reverse_word(word)
   end
-  return reverse_arr.join(" ")
+  return reversed.join(" ")
 end
